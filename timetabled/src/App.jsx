0b7457ae245f73dc18b4fb56ad38fa1fa8dddb9e
abbycakes02 <114423578+abbycakes02@users.noreply.github.com>
2023-09-12 16:12:15 -0700

    first commit

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Calendar from './components/Calendar'
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>
+      Mom's Weekly Calendar: A Recipe for a Well-Managed Week!🏞️
+      </h1>
+      <h2>
+      Exploring the Everyday World of My Amazing Mom!🐐
+      </h2>
+      <Calendar />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import Event from './Event'
+//creating react component;
+const Calendar = () => {
+    return (
+<div className="Calendar">
+<table>
+        <thead>
+            <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td className="time">8am</td>
+                <Event event='Fancy Dinner 🎩' location ='Maple & Ash' color ='pink'/>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">9 am</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">10am</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">11 am</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">12 pm</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">1 pm</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">2 pm</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">3 pm</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">4 pm</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td className="time">5 pm</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+      </table>
+</div>
+        )
+}
+//export react component;
+export default Calendar;
